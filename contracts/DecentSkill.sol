7c9a0d2b2a657e0ab73250ee8ac375471f5477b5
Zoheb Alli Khan <understandingzak@gmail.com>
2023-01-09 23:10:41 +0530

    Skill Verification

--- contracts/DecentSkill.sol
+++ contracts/DecentSkill.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.8.1;
 
 contract DecentSkill {
+    constructor() {
+        user storage dummy_user = employees.push();
+        dummy_user.name = "dummy";
+        dummy_user.wallet_address = msg.sender;
+        dummy_user.id = 0;
+        dummy_user.user_skills = new uint256[](0);
+        dummy_user.user_work_experience = new uint256[](0);
+    }
+
     company[] public companies;
     user[] public employees;
     certificate[] public certifications;
@@ -12,6 +21,11 @@ contract DecentSkill {
     mapping(string => address) public email_to_address;
     mapping(address => uint256) public address_to_id;
     mapping(address => bool) public is_company;
+
+    modifier verifiedUser(uint256 user_id) {
+        require(user_id == address_to_id[msg.sender]);
+        _;
+    }
 
     function sign_up(
         string calldata email,
@@ -50,10 +64,9 @@ contract DecentSkill {
         return (is_company[msg.sender]) ? "company" : "user";
     }
 
-    function update_wallet_address(
-        string calldata email,
-        address new_address
-    ) public {
+    function update_wallet_address(string calldata email, address new_address)
+        public
+    {
         require(
             email_to_address[email] == msg.sender,
             "error: function called from incorrect wallet address"
@@ -64,17 +77,133 @@ contract DecentSkill {
         address_to_id[new_address] = id;
     }
 
-    function memcmp(
-        bytes memory a,
-        bytes memory b
-    ) internal pure returns (bool) {
+    function add_experience(
+        uint256 user_id,
+        string calldata starting_date,
+        string calldata role,
+        string calldata ending_date,
+        uint256 company_id
+    ) public verifiedUser(user_id) {
+        experience storage new_experience = experiences.push();
+        new_experience.company_id = company_id;
+        new_experience.currently_working = false;
+        new_experience.is_approved = false;
+        new_experience.starting_date = starting_date;
+        new_experience.role = role;
+        new_experience.ending_date = ending_date;
+        employees[user_id].user_work_experience.push(experiences.length - 1);
+        companies[company_id].requested_employees.push(experiences.length - 1);
+    }
+
+    function approve_manager(uint256 employee_id) public {
+        require(is_company[msg.sender], "error: sender not a company account");
+        require(
+            employees[employee_id].company_id == address_to_id[msg.sender],
+            "error: user not of the company"
+        );
+        require(
+            !(employees[employee_id].is_manager),
+            "error: user is already a manager"
+        );
+        employees[employee_id].is_manager = true;
+    }
+
+    function add_skill(uint256 userid, string calldata skill_name)
+        public
+        verifiedUser(userid)
+    {
+        // the modifier that we created above
+        skill storage new_skill = skills.push();
+        employees[userid].user_skills.push(skills.length - 1);
+        new_skill.name = skill_name;
+        new_skill.verified = false;
+        new_skill.skill_certifications = new uint256[](0);
+        new_skill.skill_endorsements = new uint256[](0);
+    }
+
+    function add_certification(
+        uint256 user_id,
+        string calldata url,
+        string calldata issue_date,
+        string calldata valid_till,
+        string calldata name,
+        string calldata issuer,
+        uint256 linked_skill_id
+    ) public verifiedUser(user_id) {
+        certificate storage new_certificate = certifications.push();
+        new_certificate.url = url;
+        new_certificate.issue_date = issue_date;
+        new_certificate.valid_till = valid_till;
+        new_certificate.name = name;
+        new_certificate.id = certifications.length - 1;
+        new_certificate.issuer = issuer;
+        skills[linked_skill_id].skill_certifications.push(new_certificate.id);
+    }
+
+    function endorse_skill(
+        uint256 user_id,
+        uint256 skill_id,
+        string calldata endorsing_date,
+        string calldata comment
+    ) public {
+        endorsment storage new_endorsemnt = endorsments.push();
+        new_endorsemnt.endorser_id = address_to_id[msg.sender];
+        new_endorsemnt.comment = comment;
+        new_endorsemnt.date = endorsing_date;
+        skills[skill_id].skill_endorsements.push(endorsments.length - 1);
+        if (employees[address_to_id[msg.sender]].is_manager) {
+            if (
+                employees[address_to_id[msg.sender]].company_id ==
+                employees[user_id].company_id
+            ) {
+                skills[skill_id].verified = true;
+            }
+        }
+    }
+
+    function approve_experience(uint256 exp_id, uint256 company_id) public {
+        require(
+            (is_company[msg.sender] &&
+                companies[address_to_id[msg.sender]].id ==
+                experiences[exp_id].company_id) ||
+                (employees[address_to_id[msg.sender]].is_manager &&
+                    employees[address_to_id[msg.sender]].company_id ==
+                    experiences[exp_id].company_id),
+            "error: approver should be the company account or a manager of the required company"
+        );
+        uint256 i;
+        experiences[exp_id].is_approved = true;
+        for (i = 0; i < companies[company_id].requested_employees.length; i++) {
+            if (companies[company_id].requested_employees[i] == exp_id) {
+                companies[company_id].requested_employees[i] = 0;
+                break;
+            }
+        }
+
+        for (i = 0; i < companies[company_id].current_employees.length; i++) {
+            if (companies[company_id].current_employees[i] == 0) {
+                companies[company_id].requested_employees[i] = exp_id;
+                break;
+            }
+        }
+        if (i == companies[company_id].current_employees.length) {
+            companies[company_id].current_employees.push(exp_id);
+        }
+    }
+
+    function memcmp(bytes memory a, bytes memory b)
+        internal
+        pure
+        returns (bool)
+    {
         return (a.length == b.length) && (keccak256(a) == keccak256(b));
     }
 
-    function strcmp(
-        string memory a,
-        string memory b
-    ) internal pure returns (bool) {
+    function strcmp(string memory a, string memory b)
+        internal
+        pure
+        returns (bool)
+    {
         return memcmp(bytes(a), bytes(b));
     }
 }
@@ -105,7 +234,7 @@ struct experience {
     string role;
     bool currently_working;
     uint256 company_id;
-    bool is_improved;
+    bool is_approved;
 }
 
 struct certificate {
