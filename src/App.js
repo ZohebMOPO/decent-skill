2048c090bef0759d3fb0f5cc5cb15f5aa2ee381a
Zoheb Alli Khan <understandingzak@gmail.com>
2023-01-10 15:07:58 +0530

    Dn!!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import Web3 from "web3";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setState] = useState({
+    web3: null,
+    contract: null,
+    email: "",
+    account: "",
+    accountId: "",
+    signedIn: false,
+    loaded: false,
+  });
+
+  const initEth = async () => {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    if (window.ethereum) {
+      try {
+        const web3 = new Web3(window.ethereum);
+        const account = (await web3.eth.getAccounts())[0];
+        const netId = await web3.eth.net.getId();
+        const address = SmartContract.networks[netId].address;
+        const contract = new web3.eth.Contract(SmartContract.abi, address);
+        const accountId = await contract.methods.address_to_id(account).call();
+        setState({
+          ...state,
+          web3,
+          account,
+          contract,
+          accountId,
+          loaded: true,
+        });
+        console.log("setup complete");
+      } catch (err) {
+        alert(err);
+      }
+    } else {
+      alert("ethereum not detected");
+    }
+  };
+
+  const login = async () => {
+    try {
+      const accountType = await state.contract.methods.login(state.email).call({
+        from: state.account,
+      });
+      console.log("account type:", accountType);
+      setState({ ...state, signedIn: true });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const signUp = async () => {
+    try {
+      await state.contract.methods
+        .sign_up(state.email, "name", "user")
+        .send({ from: state.account });
+      alert("signed up");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const requestCompany = async (startDate, endDate, role, companyId) => {
+    try {
+      await state.contract.methods.add_experience(
+        state.accountId,
+        startDate,
+        endDate,
+        role,
+        companyId
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const approveEmployee = async (experienceId, companyId) => {
+    try {
+      await state.contract.methods.approve_experience(experienceId, companyId);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const updateWallet = async (newAddress) => {
+    try {
+      await state.contract.methods.update_wallet_address(
+        state.email,
+        newAddress
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const approveManager = async (empId) => {
+    try {
+      await state.contract.methods.approve_manager(empId);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const addCertificate = async (
+    certUrl,
+    issueDate,
+    validTill,
+    certName,
+    issuer,
+    linkedSkill
+  ) => {
+    try {
+      await state.contract.methods.add_certification(
+        state.accountId,
+        certUrl,
+        issueDate,
+        validTill,
+        certName,
+        issuer,
+        linkedSkill
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const addSkill = async (skillName) => {
+    try {
+      await state.contract.methods.add_skill(state.accountId, skillName);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const endorseSkill = async (empId, skillId, comment) => {
+    const date = new Date();
+    try {
+      await state.contract.methods.endorse_skill(
+        empId,
+        skillId,
+        `${date.getMonth()} ${date.getFullYear()}`,
+        comment
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return <div className="App"></div>;
 }
 
 export default App;
